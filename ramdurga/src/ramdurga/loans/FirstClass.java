75eae8b7e5ce9c089b3b8600774243a498436a10
Durga30492 <durga7757@gmail.com>
2020-05-17 17:59:52 +0530

    added a b c variables

--- ramdurga/src/ramdurga/loans/FirstClass.java
+++ ramdurga/src/ramdurga/loans/FirstClass.java
@@ -1,7 +1,7 @@
 package ramdurga.loans;
 
 public class FirstClass {
-
+int a=10,b=20,c;
 	public static void main(String[] args) {
 		System.out.println("i am new class");
 		System.out.println("i am new classadded");
